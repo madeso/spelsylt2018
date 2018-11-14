d1b069817ddf13ab6ac07305108eb509eb22befd
Gustav <sir.gustav.the.coder@gmail.com>
2018-11-14 15:40:51 +0100

    loaded test level

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
-love.graphics.setDefaultFilter("nearest", "nearest")
-sprites = love.graphics.newImage("sprites.png")
+local sti = require "sti"
 
 make_sprite = function(x)
   return love.graphics.newQuad(x*32, 0, 32, 32, sprites:getWidth(), sprites:getHeight())
@@ -9,22 +8,35 @@ draw_sprite = function(q, x, y)
   love.graphics.draw(sprites, q, x, y)
 end
 
-idle_sprite = make_sprite(0)
-
-love.load = function()
+load_level = function(path)
+  level_gfx = sti(path)
+  player.x = 90
+  player.y = 0
 end
+
+--------------------------------------------------------------
 
 LIGHT_BG = {r= 196, g= 208, b= 162}
 DEFAULT_BG = {r= 131, g= 142, b= 102}
 
+love.graphics.setDefaultFilter("nearest", "nearest")
+sprites = love.graphics.newImage("sprites.png")
+idle_sprite = make_sprite(0)
+player = {x=0, y=0}
+load_level("level1.lua")
+--------------------------------------------------------------
+
+love.load = function()
+end
+
 love.draw = function()
-  -- love.graphics.print("Yo World!", 400, 300)
   local set_background = function(c)
     local max = 255
     love.graphics.setBackgroundColor(c.r/max, c.g/max, c.b/max)
   end
   set_background(DEFAULT_BG)
-  draw_sprite(idle_sprite, 20, 20)
+  level_gfx:draw()
+  draw_sprite(idle_sprite, player.x, player.y)
 end
 
 love.update = function()
